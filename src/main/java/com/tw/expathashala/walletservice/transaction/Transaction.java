da14f860bc1351ff30005897acb9f9de8675b1cf
pratibha.tiwari <pratibha.tiwari@thoughtworks.com>
2019-04-05 15:22:03 +0530

    [Ved/Pratibha] change fetch-type to lazy for transactions

--- src/main/java/com/tw/expathashala/walletservice/transaction/Transaction.java
+++ src/main/java/com/tw/expathashala/walletservice/transaction/Transaction.java
@@ -26,7 +26,7 @@ public class Transaction {
     @Enumerated(EnumType.STRING)
     private TransactionType type;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "wallet_id")
     @JsonIgnore
     private Wallet wallet;
